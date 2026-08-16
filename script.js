893dd8191d4785ae8556b61430d251bc603c8ef8
ikerdlv07 <ikerdlv07@gmail.com>
2026-08-16 13:04:02 +0200

    Added Diabetes-menu to connect the article among them. Moreover, I have done a little update of the web

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-cons button = document.querySelector("button");
-button.onclick = function(){
-alert("Hello!");    
-}
-
 const dropdownButton = document.querySelector(".dropdown-button");
 const dropdown = document.querySelector(".dropdown");
 
